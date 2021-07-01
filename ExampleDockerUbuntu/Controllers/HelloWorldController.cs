26fb3099352c596f1c9ea4aac67640afea7783aa
demoAPI <viktor.raymundo@gmail.com>
2021-07-01 11:11:08 -0700

    Add project files.

---
+++ ExampleDockerUbuntu/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleDockerUbuntu.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world, using docker in ubuntu";
+        }
+    }
+}
