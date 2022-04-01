4649326a183e3a65b9d3975e45c6d88a08bdf281
Viktor <47989283+VictorRH@users.noreply.github.com>
2022-04-01 10:54:52 -0700

    update net core 6

--- ExampleDockerUbuntu/Program.cs
+++ ExampleDockerUbuntu/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +21,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
